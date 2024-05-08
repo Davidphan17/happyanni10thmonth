b56e8316e40e7bee4b23cba29960b3d95cbb6255
Ngoc Anh Tam Phan <91599328+Davidphan17@users.noreply.github.com>
2024-05-09 02:17:44 +1000

    config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hello babiboo của anh 🖐🖐🖐',
-    introDesc: `Hôm nay là 7/10/2021 anni 10th month của tụi mình anh có vài lời muốn gửi gắm tới cục cưng của anhhh`,
-    btnIntro: 'bấm vào đây nè bé ơi',
-    title: 'Em biết anh yêu em nhiều lắm hongggg :3',
-    desc: 'Em à nhắm mắt nhắm mũi va vào anh cũng được 10 tháng ròi đó hehe. Nhanh ghê ha mới đây mà đã quen nhau được 10 tháng mới ngày nào còn ngại ngùng hong dám hun má hê hê  như vậy là tụi mình đã yêu nhao được 10 tháng ròi, 10 tháng nhưng mà thiệt ra chắc phải yêu xa hết 5 tháng ròiiiii nhma thôi hogn sao vì yêu nhao mình vượt qua hết hê hê. Trong 10 tháng qua mình đã cũng nhao vượt qua bao nhiu là khó khăn nhma vẫn cùng nhau vượt qua và bên nhau đến bây h anh yêu bé nhiều lắm sắp tới em cbi đi du học rồi haizzz lại thêm thời gian yêu xa nhma thoi vì yêu mình phải cố gắng hehe anh sẽ cố gắng hết sức để có thể đi du học cùng em nha hẹn bà xã ở bên Úc và cùng nhau thực hiện nhiều thức và mục tiêu chúng mình đã đặt ra với nhau nha em yeuuu thương em nhứt luon á   ',
-    btnYes: 'em cũng yêu anh nhiều nhắmmm 💖💖💖',
-    btnNo: '💔 Ewwww no ! 💔 ',
-    question:'Nói cho Anh biết đi cục cưng có muốn anh thay đổi gì để có thể cùng nhau hongggg 😊😊',
-    reasonPlaceholder: 'tsaooo dợ',
-    btnReply: 'Gởi ! 💌💌',
-    reply: ' Hoi em chỉ cần anh là chính anh hoi là được ròi😊😊',
-    mess: 'Anh biết mà hê hê anh lúc nèo cũng muốn bé vui hong được buồn vì em xinh nhất khi cười mòo 🥰. Love you 300.000 happy annivasary 10th month babiboo 😘😘',
-    messDesc: '💬💬 Bé có thể gửi gắm lời yêu thương của bé đến cho anhhh nè ntin qua mess ó. 💬💬',
-    btnAccept: 'Yêu em bé của anh 💖',
-    messLink: 'https://m.me/anhtam.phanngoc'
+    introTitle: ' ', //write the intro title
+    introDesc: ``,
+    btnIntro: '',// write a message to tell her to press this one
+    title: '',// write some love message
+    desc: '', // write some love message
+    btnYes: '', // write a decision for her
+    btnNo: '', // write a decision for her
+    question:'',// asking her about what she wants you to change in the future
+    reasonPlaceholder: '',// the reason why she wants that
+    btnReply: '! 💌💌',// sent
+    reply: ' ',// write a short response 
+    mess: '',//write some love message
+    messDesc: '', // told her to send any message she wants to send you through Facebook
+    btnAccept: '',//write some love message
+    messLink: ''// link your social media profile
 }
